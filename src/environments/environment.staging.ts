149cf72b19df6117e581e883e208183964c482ea
alex-poinot <77551076+alex-poinot@users.noreply.github.com>
2025-09-11 13:31:30 +0200

    Mise à jour des URLs vers myjourney-test.grant-thornton.fr

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: false,
   name: 'staging',
-  apiUrl: 'http://10.100.9.40:3000',
+  apiUrl: 'https://myjourney-test.grant-thornton.fr/api/myjourney',
   azure: {
     clientId: '61afef70-d6d1-45cd-a015-82063f882824',
     tenantId: 'e1029da6-a2e7-449b-b816-9dd31f7c2d83',
-    redirectUri: 'http://10.100.9.40/',
-    postLogoutRedirectUri: 'http://10.100.9.40/'
+    redirectUri: 'https://myjourney-test.grant-thornton.fr/myjourney/',
+    postLogoutRedirectUri: 'https://myjourney-test.grant-thornton.fr/myjourney/'
   },
   features: {
     enableLogging: true,
